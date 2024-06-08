40c9bb56b24c01924fb40105d5b90eeefabc5972
ambroseonapa <150067911+ambroseonapa@users.noreply.github.com>
2024-06-08 12:58:35 +0300

    Add files via upload

---
+++ c programming assignment no 1.c
@@ -0,0 +1,20 @@
+
+/*question 1*/
+
+#include<stdio.h>
+
+/*The program that is debugged*/
+int x;
+scanf("&d",x);
+int main()
+{
+    if(x==1)
+    {
+        printf("x equals 1\n");
+    }
+    else
+    {
+        printf("x does not equal to 1\n");
+    }
+    return 0;
+}
